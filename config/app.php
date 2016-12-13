fede135f36883e6c95a5ef8210662850f7d0b32d
Ihsan Arif Rahman <ihsan_arif@apps.ipb.ac.id>
2016-12-13 22:02:53 +0700

    add Laratrust role user

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Yajra\Datatables\DatatablesServiceProvider::class,
+        Laratrust\LaratrustServiceProvider::class,
 
     ],
 
@@ -229,6 +230,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Laratrust'=> Laratrust\LaratrustFacade::class,
     ],
 
 ];
