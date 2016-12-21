befa8c4a6993bbecbee2f0231730d424f9fce53c
Ihsan Arif Rahman <ihsan_arif@apps.ipb.ac.id>
2016-12-21 14:00:59 +0700

    view import feeds

--- resources/views/feeds/create.blade.php
+++ resources/views/feeds/create.blade.php
@@ -13,10 +13,32 @@
             <h2 class="panel-title">Create Feed Stuff</h2>
         </div>
         <div class="panel-body">
-        {!! Form::open(['url' => route('feeds.store'), 'method' => 'post', 'class'=>'form-horizontal']) !!} 
-        @include('feeds._form')
-        {!! Form::close() !!}
-          </div>
+          <ul class="nav nav-tabs" role="tablist">
+                <li role="presentation" class="active">
+                  <a href="#form" aria-controls="form" role="tab" data-toggle="tab">
+                    <i class="fa fa-pencil-square-o"></i> Isi Form
+                </a> </li>
+                <li role="presentation">
+                  <a href="#upload" aria-controls="upload" role="tab" data-toggle="tab">
+                    <i class="fa fa-cloud-upload"></i> Upload Excel
+                  </a>
+              </li> 
+            </ul>
+            <div class="tab-content">
+              <div role="tabpanel" class="tab-pane active" id="form">
+                <br><br>
+                {!! Form::open(['url' => route('feeds.store'), 'method' => 'post', 'class'=>'form-horizontal']) !!} 
+                  @include('feeds._form')
+                {!! Form::close() !!}
+              </div>
+             <div role="tabpanel" class="tab-pane" id="upload">
+                <br><br>
+                {!! Form::open(['url' => route('feeds.store'),
+                'method' => 'post', 'files'=>'true', 'class'=>'form-horizontal']) !!}
+                @include('feeds._import')
+                {!! Form::close() !!}
+              </div>
+            </div>
         </div>
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,14 @@ Route::group(['middleware'=>'web'],function(){
     Route::group(['prefix'=>'admin','middleware'=>['auth','role:admin']],function(){
         Route::resource('groupfeeds','GroupFeedsController');
         Route::resource('feeds','FeedsController');
+
+        Route::get('template/feeds', [
+            'as'   => 'template.feeds',
+            'uses' => 'FeedsController@generateExcelTemplate'
+        ]);
     });
 });
+
 Route::get('/welcome', 'HomeController@home');
 Route::post('/input','HomeController@input');
 Route::post('/calculate','HomeController@calculate_using_minimization_class');
