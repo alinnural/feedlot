19daa684c2fb6e9784ffb4844ea69001ed7e56f1
Ihsan Arif Rahman <ihsan_arif@apps.ipb.ac.id>
2017-03-06 10:05:41 +0700

    adding class curl for github repository

---
+++ app/Helpers/Curl.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\helpers;
+
+class Curl{
+    
+    public static function curl_get_contents($url)
+    {
+        $ch = curl_init();
+        $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)';
+        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
