d5e912331a30cb140d59cf8aabffb5c37e8adbdd
Ihsan Arif Rahman <ihsan_arif@apps.ipb.ac.id>
2017-11-16 17:10:23 +0700

    social media editor

---
+++ resources/views/admin/social/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('title')
+  Editor Social Media - Admin {{ config('configuration.site_name') }}
+@endsection
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <ul class="breadcrumb">
+          <li><a href="{{ url('/admin') }}">Dashboard</a></li>
+          <li class="active">Editor Sosial Media</li>
+        </ul>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <div class="btn-group pull-right">
+                <a class="btn btn-primary btn-sm" href="{{ url('admin/social/create') }}"><i class="fa fa-pencil"></i> Create</a>
+            </div>
+            <h2 class="panel-title" style="padding-bottom:5px;padding-top:5px;"> Social Media</h2>
+          </div>
+          <div class="panel-body">
+            {!! $html->table(['class'=>'table table-striped table-hover']) !!}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('scripts')
+  {!! $html->scripts() !!}
+@endsection
