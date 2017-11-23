fe397983cd261a803dfdc2dc2e78d138ec38c73d
Ihsan Arif Rahman <ihsan_arif@apps.ipb.ac.id>
2017-11-23 17:07:37 +0700

    warning service provider

--- app/Providers/ConfigurationServiceProvider.php
+++ app/Providers/ConfigurationServiceProvider.php
@@ -13,6 +13,10 @@ class ConfigurationServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /*
+        information : https://stackoverflow.com/questions/40794966/laravel-settings-service-provider-database-migration
+        baris ini dikomentari dulu saat mau migrate karena akan ada pesan error
+        */
         config()->set('configuration', \App\Setting::pluck('value', 'code')->all());
     }
 
