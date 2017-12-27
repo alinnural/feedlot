40124480bdaf11a78473ef60883e7d4547ad1d98
Ihsan Arif Rahman <ihsanarifr@gmail.com>
2017-12-27 16:03:39 +0700

    question answer model

---
+++ app/Answer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    // Add the following near the top of the class, after $dates
+    protected $fillable = [
+      'title', 'name','email', 'description'
+    ];
+
+    public function question()
+    {
+        return $this->belongTo('App\Question');
+    }
+}
---
+++ database/migrations/2017_12_27_084151_create_jawaban.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJawaban extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('answer');
+            $table->timestamps();
+
+            $table->foreign('question_id')->references('id')->on('questions')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
