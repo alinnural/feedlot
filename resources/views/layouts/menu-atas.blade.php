385e569e45360c5d68bbf583c5299ee8dd5481ee
Ihsan Arif Rahman <ihsanarifr@gmail.com>
2017-12-31 09:50:33 +0700

    order child by position

--- resources/views/layouts/menu-atas.blade.php
+++ resources/views/layouts/menu-atas.blade.php
@@ -8,7 +8,7 @@ $menu = App\Menu::IsParentParentID()->orderBy('position','asc')->get();
             <a href="{{ url($m['url']) }}" class="dropdown-toggle" data-toggle="dropdown">{{ $m['name'] }} <span class="caret"></span></a>
             
             <ul class="dropdown-menu">
-                @php $child = App\Menu::Where('parent_id',$m['id'])->get(); @endphp
+                @php $child = App\Menu::Where('parent_id',$m['id'])->orderBy('position','asc')->get(); @endphp
                 @foreach($child as $c)
                     <li class="menu-item"><a href="{{ url($c['url']) }}">{{ $c['name'] }}</a></li>
                 @endforeach
