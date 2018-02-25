42013f5e02fd2afe401b4df5271799443c258f62
Ihsan Arif Rahman <ihsanarifr@gmail.com>
2018-02-26 04:27:48 +0700

    update answer and album

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
@@ -23,25 +24,26 @@ class QuestionController extends Controller
             return Datatables::of($question)->addColumn('action', function ($question) {
                 return view('admin.question._action', [
                     'model' => $question,
+                    'edit_answer' => url('admin/question/edit_answer/' . $question->id),
                     'edit_url' => route('question.edit', $question->id),
                     'delete_url' => route('question.destroy', $question->id),
                     'confirm_message' => 'Are you sure to delete ' . $question->title . '?'
                 ]);
             })
                 ->addColumn('created_at', function ($question) {
-                $created = new Carbon($question->created_at);
-                $now = Carbon::now();
-                
-                $difference = ($created->diff($now)->days < 2) ? $created->diffForHumans($now) : $created;
-                return $difference;
-            })
+                    $created = new Carbon($question->created_at);
+                    $now = Carbon::now();
+
+                    $difference = ($created->diff($now)->days < 2) ? $created->diffForHumans($now) : $created;
+                    return $difference;
+                })
                 ->addColumn('name_email', function ($question) {
-                return $question->name . ' ' . $question->email;
-            })
+                    return $question->name . ' ' . $question->email;
+                })
                 ->addColumn('is_answered', function ($question) {
-                return $question->answers()
-                    ->count() == 0 ? "<label class='label label-warning'>Draft</label>" : "<label class='label label-success'>Answered</label>";
-            })
+                    return $question->answers()
+                        ->count() == 0 ? "<label class='label label-warning'>Draft</label>" : "<label class='label label-success'>Answered</label>";
+                })
                 ->make(true);
         }
         $html = $htmlBuilder->addColumn([
@@ -53,31 +55,31 @@ class QuestionController extends Controller
             'width' => 150
         ])
             ->addColumn([
-            'data' => 'title',
-            'name' => 'title',
-            'title' => 'Judul',
-            'width' => 200
-        ])
+                'data' => 'title',
+                'name' => 'title',
+                'title' => 'Judul',
+                'width' => 200
+            ])
             ->addColumn([
-            'data' => 'name_email',
-            'name' => 'name_email',
-            'title' => 'Nama',
-            'width' => 30
-        ])
+                'data' => 'name_email',
+                'name' => 'name_email',
+                'title' => 'Nama',
+                'width' => 30
+            ])
             ->addColumn([
-            'data' => 'is_answered',
-            'name' => 'is_answered',
-            'title' => 'Status',
-            'width' => 30
-        ])
+                'data' => 'is_answered',
+                'name' => 'is_answered',
+                'title' => 'Status',
+                'width' => 30
+            ])
             ->addColumn([
-            'data' => 'action',
-            'name' => 'action',
-            'title' => '',
-            'orderable' => false,
-            'searchable' => false,
-            'width' => 150
-        ]);
+                'data' => 'action',
+                'name' => 'action',
+                'title' => '',
+                'orderable' => false,
+                'searchable' => false,
+                'width' => 150
+            ]);
         return view('admin.question.index')->with(compact('html'));
     }
 
@@ -92,16 +94,16 @@ class QuestionController extends Controller
         $this->validate($request, [
             'answer' => 'required'
         ]);
-        
+
         $question = Question::find($id);
         $answer = new Answer();
-        
+
         if ($question) {
             $answer->answer = $request->answer;
             $answer->question()->associate($question);
             $answer->user()->associate(Auth::user());
             $answer->save();
-            
+
             Session::flash("flash_notification", [
                 "level" => "success",
                 "message" => "Berhasil menjawab pertanyaan $question->title"
@@ -115,19 +117,31 @@ class QuestionController extends Controller
     public function destroy(Request $request, $id)
     {
         $question = Question::findOrFail($id);
-        
+
         if ($question) {
             $question->answers()->delete();
             $question->delete();
         } else {
             return redirect()->back();
         }
-        
+
         Session::flash("flash_notification", [
             "level" => "success",
             "message" => "Pertanyaan berhasil dihapus"
         ]);
-        
+
         return redirect('admin/question');
+    }
+
+    public function edit_answer($id)
+    {
+        $answer = Answer::findOrFail($id);
+        $question = $answer->question()->first();
+        return view('admin.question.edit_answer')->with(compact('answer'))->with(compact('question'));
+    }
+
+    public function update_answer(Request $request, $id)
+    {
+
     }
 }
--- resources/views/admin/question/_action.blade.php
+++ resources/views/admin/question/_action.blade.php
@@ -1,4 +1,8 @@
 {!! Form::model($model, ['url' => $delete_url, 'method' => 'delete', 'class' => 'form-inline js-confirm', 'data-confirm' => $confirm_message ]) !!}
-    <a href="{{ $edit_url }}" class="btn btn-xs btn-success"><i class="fa fa-pencil"></i> Jawab</a>
+	@if($model->answers()->count() == 0)
+    		<a href="{{ $edit_url }}" class="btn btn-xs btn-success"><i class="fa fa-pencil"></i> Jawab</a>
+    	@else
+    		<a href="{{ $edit_answer }}" class="btn btn-xs btn-warning"><i class="fa fa-pencil"></i>Edit</a>
+    	@endif
     {!! Form::button('<i class="fa fa-trash"></i> Hapus', ['class'=>'btn btn-xs btn-danger','type'=>'submit']) !!}
 {!! Form::close()!!}
